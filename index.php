f2ee07de2437028928d5877721b9acc23c940285
jason <thorin904@yahoo.com>
2015-04-28 15:18:57 -0400

    implemented a bubble sort in index.php

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php
 
-	function greet($name){
-		echo "Hello " . $name . ".<br>";
-	}
-
-	function printArray($array){
+	// bubble sort
+	function doBubbleSort($array){
 		for ($i = 0; $i < sizeof($array); $i++){
-			echo "index " . ($i + 1) . ": " . $array[$i] . "<br>";
+			for ($j = 0; $j < sizeof($array) - 1; $j++){
+				if ($array[$j] > $array[$j + 1]){
+					$temp = $array[$j];
+					$array[$j] = $array[$j + 1];
+					$array[$j + 1] = $temp;
+				}
+			}
 		}
+		
+		return $array;
 	}
 	
-	greet("Jason");
+	// function for printing an array
+	function printArray($array){
+		echo "{";
+		for ($i = 0; $i < sizeof($array); $i++){
+			echo $array[$i];
+			if ($i < sizeof($array) - 1){
+				echo ", ";
+			}
+		}
+		echo "}<br>";
+	}
+	
+	// main method
+	$numArray = array(99, 54, 8, 19, 25, 1, 42, 37, 3);
+	
+	echo "unsorted array: ";
+	printArray($numArray);
 
-	$names = array("Jason", "Milena", "Isabel", "Spike");
-	printArray($names);
-
+	$numArray = doBubbleSort($numArray);
+	
+	echo "sorted array: ";
+	printArray($numArray);
 ?>
