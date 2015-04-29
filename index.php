04563ad72313e02c25cd940b7c5f23a6bcc4fa32
jason <thorin904@yahoo.com>
2015-04-28 22:40:07 -0400

    new files: Array and testing, modified: index and random

--- index.php
+++ index.php
@@ -15,6 +15,17 @@
 		return $array;
 	}
 	
+	// for creation of array size $size with random values from $min to $max
+	function createRandomArray($min, $max, $size){
+		$numArray = array();
+		
+		for ($i = 0; $i < $size; $i++){
+			array_push($numArray, rand($min, $max));
+		}
+		
+		return $numArray;
+	}
+	
 	// function for printing an array
 	function printArray($array){
 		echo "{";
@@ -28,13 +39,46 @@
 	}
 	
 	// main method
-	$numArray = array(99, 54, 8, 19, 25, 1, 42, 37, 3);
+	$numArray = createRandomArray(1, 1000, 1000); 
+	$numArray2 = $numArray;
+	echo "array size: " . sizeof($numArray) . "<br>";
 	
-	echo "unsorted array: ";
-	printArray($numArray);
+// 	echo "unsorted array: ";
+// 	printArray($numArray);
+	
+	/***************************************************************/
+	
+	// timing the sort
+	$timeStart1 = microtime(true);
+	
+	// php built in sorting function, not sure what kind of sort they implement
+	sort($numArray);
+	
+	// end timing of the sort
+	$timeEnd1 = microtime(true);
+	
+	echo "php sort time: " . $timeEnd1 - $timeStart1 . " seconds<br>"; 
 
+	/***************************************************************/
+	
+	// testing to make sure the second array was indeed a copy and not just pointing 
+	// to the same array
+// 	printArray($numArray);
+// 	printArray($numArray2);
+	
+	// timing the sort
+	$timeStart2 = microtime(true);
+	
+	// below is a call to my own bubble sort function
 	$numArray = doBubbleSort($numArray);
 	
-	echo "sorted array: ";
-	printArray($numArray);
+	// end timing of the sort
+	$timeEnd2 = microtime(true);
+	
+	echo "bubble sort time: " . $timeEnd2 - $timeStart2 . " seconds<br>";
+	
+// 	echo "sorted array: ";
+// 	printArray($numArray);
+
+	/***************************************************************/
 ?>
